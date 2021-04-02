a70b9ae4640f86d47c698befae5f49d023a6f09e
Grace <grace_lee1@brown.edu>
2021-04-01 23:18:49 -0400

    added js animations

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    let elts = document.getElementsByClassName("accordion");
+
+    this.classList.toggle("accordion-active");
+
+    for(let j = 0; j < elts.length; j++){
+      if (elts[j] === this) {
+        continue;
+      }
+      let otherPanel = elts[j].nextElementSibling;
+      if (otherPanel.style.display === "block") {
+        otherPanel.style.display = "";
+        elts[j].classList.remove("accordion-active");
+      }
+    }
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "") {
+        panel.style.display = "block";
     } else {
-      panel.style.display = "block";
+        panel.style.display = "";
     }
   });
 }
+
+
+window.onload = () => {
+            let fade = document.getElementsByClassName("fade-in")[0];
+            let opacity = 0;
+            let intervalID = setInterval(function() {
+
+                if (opacity < 1) {
+                    opacity = opacity + 0.15
+                    fade.style.opacity = opacity;
+                } else {
+                    clearInterval(intervalID);
+                }
+            }, 85);
+        }
