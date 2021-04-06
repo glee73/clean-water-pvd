699f54486577aad8911de7ed8ba216f62ebb5160
Grace <grace_lee1@brown.edu>
2021-04-06 02:44:41 -0400

    finished with basic layout of everything

---
+++ js/modal.js
@@ -0,0 +1,29 @@
+
+let argsList = [["explainer", "explainer-link", "exit-explainer"],
+           ["presentation", "presentation-link", "exit-presentation"],
+           ["newark", "newark-link", "exit-newark"],
+           ["case-study", "case-study-link", "exit-case-study"]];
+
+argsList.map(x => setUpModal(x));
+
+function setUpModal([m, b, s]) {
+
+  let modal = document.getElementById(m);
+  let btn = document.getElementById(b);
+  let span = document.getElementById(s);
+  console.log(span);
+
+  btn.onclick = function() {
+  modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+  modal.style.display = "none";
+  }
+
+    window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+  }
+}
